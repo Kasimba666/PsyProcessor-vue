1e84bb76442b52026dc456dc17a083e95c5a8d35
Kasimba666 <kasimba666@gmail.com>
2024-03-16 01:29:00 +0300

    publicPath and history

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/PsyProcessor-vue/",
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/PsyProcessor-vue/'
+        : '/',
+    // base: "/PsyProcessor-vue/",
     plugins: [
         vue(),
         vueJsx(),
