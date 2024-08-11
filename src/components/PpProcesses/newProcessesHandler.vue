746623b53973a96c785c58e01b39e7e67db4f3ac
Kasimba666 <kasimba666@gmail.com>
2024-08-11 10:27:01 +0300

    Начало валидации процесса

--- src/components/PpProcesses/newProcessesHandler.vue
+++ src/components/PpProcesses/newProcessesHandler.vue
@@ -18,7 +18,7 @@
       <el-table-column label="Версия" width="80" align="center">
         <template #default="scope">
           <div :class="{'notLastVersion': !isLastVersion(scope.row.header.version)}">
-            {{ scope.row.header.version }}
+            {{ scope.row.header.version ?? 'нет'}}
           </div>
         </template>
       </el-table-column>
@@ -27,6 +27,17 @@
           <div :class="{'notUniqueID': isIDExist(scope.row.id)}">
             {{ scope.row.id }}
           </div>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="Валидация" width="200" align="center">
+        <template #default="scope">
+          <button
+              class="btn btn-outline-primary btn-sm"
+              @click="onCheck(scope.row)"
+          >
+            Проверить
+          </button>
         </template>
       </el-table-column>
 
@@ -52,10 +63,13 @@
         </template>
       </el-table-column>
     </el-table>
+    <div class="logs">
+        {{logs}}
+    </div>
     <div class="footer">
       <button
           class="btn btn-outline-primary btn-sm"
-          @click="onClose"
+          @click="onClose()"
       >
         Закрыть
       </button>
@@ -74,6 +88,7 @@ export default {
   emits: ['update:visibleNewProcesses'],
   data() {
     return {
+      logs: '',
       processes: null,
     }
   },
@@ -83,7 +98,46 @@ export default {
     ...mapMutations(['addProcessesInList']),
 
   },
+  setup() {
+    const processVersions = {
+      '0.0.0': {
 
+      },
+      '0.0.1': {
+        'id': {type: 'string', required: true},
+        'header': {type: 'object', required: true, children: {
+            'processTitle': {type: 'string', required: true},
+            'version': {type: 'string', required: true},
+            'processCategory': {type: 'string',required: true},
+            'createdDt': {type: 'dateTime', required: true},
+            'changedDt': {type: 'dateTime', required: true},
+            'description': {type: 'string', required: true}}
+        },
+        'type': {type: 'string', required: true},
+        'vars': {type: 'array', required: true, children: [
+
+          ],
+        }
+      },
+      '0.0.2': {
+        'id': {
+          default: 'function',
+          required: true,
+          type: 'string'
+        },
+        'type': {
+          default: 'process',
+          required: true,
+          type: 'string'
+        },
+      },
+
+     }
+    ;
+    return {
+      processVersions
+    }
+  },
   methods: {
     isIDExist(v) {
       return !!this.processesByID[v];
@@ -94,12 +148,53 @@ export default {
     isLastVersion(v) {
       return v === this.version;
     },
+
+    isValid(v) {
+
+    },
+
     onClickLoad(v) {
       this.$store.commit('addProcessesInList', v);
     },
+    checkField(v, field, type, required) {
+      let isValid = true;
+      let report = field + ': ';
+      if (v.hasOwnProperty(field)) {
+        report += ' существует; ';
+        if (typeof(v[field]) === type) {
+          report += typeof(v[field]) + ' тип верный; ';
+          if (required && (!!v[field])) {
+            report += 'значение верно.';
+          } else {
+            report += 'значения нет.';
+            isValid = false;
+          }
+        } else {
+          report += 'тип неверный; ';
+          isValid = false;
+        }
+      } else {
+        report += 'не существует; ';
+        isValid = false;
+      }
+      return {isValid, report}
+    },
+    onCheck(v) {
+      this.logs=[];
+      if (!v.header.hasOwnProperty('version') && !v.hasOwnProperty('version') ) {
+        this.logs='Нет поля "версия"';
+        return;
+      }
+      if (v.header.version === '0.0.1') {
+        this.logs.push(this.checkField(v, 'id', 'string', true).report);
+        this.logs.push(this.checkField(v, 'header', 'object', true).report);
+        this.logs.push(this.checkField(v.header, 'processTitle', 'string', true).report);
+        this.logs.push(this.checkField(v.header, 'createdDt', 'string', true).report);
+
+      }; //'0.0.1'
+    },
 
     onClose() {
-
       this.$emit('update:visibleNewProcesses', false);
     }
 
@@ -119,6 +214,10 @@ export default {
     height: auto;
     border: 1px solid blue;
     background-color: white;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
     overflow: auto;
     .notLastVersion {
       background-color: hsl(0, 61%, 86%);
@@ -133,6 +232,13 @@ export default {
       display: flex;
       justify-content: start;
       align-items: center;
+    }
+    .logs {
+      width: 95%;
+      height: auto;
+      padding: 5px;
+      margin-top: 5px;
+      border: 1px solid hsl(0, 0%, 50%);
     }
     .footer {
      width: 100%;
